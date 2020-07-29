03428efdb7f93aa950bf25ba788ae81e3bb7bd96
Frederico <fredrumond@gmail.com>
2020-07-29 18:26:31 -0300

    Padronizando alerta de erros de time e cliente

--- resources/views/admin/time/index.blade.php
+++ resources/views/admin/time/index.blade.php
@@ -46,6 +46,7 @@
 						<div class="form-group col-md-12">
 							<label for="nome">Nome</label>
 							<input type="text" class="form-control" name="nome" id="nome">
+							<div class="feedback"></div>
 						</div>								
 					</div>
 					<button type="submit" class="btn btn-primary">Enviar</button>
